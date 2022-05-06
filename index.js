157d1ac75679e20af90ee7f2b8101f614a8fe35a
Shafak93 <shafakctg64@gmail.com>
2022-05-07 01:53:38 +0600

    get all product api and single product api create

--- index.js
+++ index.js
@@ -12,6 +12,36 @@ app.get('/', (req, res)=>{
 });
 
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PSS}@cluster0.axhwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+        await client.connect();
+        const bikeCollection = client.db('bikeDB').collection('bike');
+
+        app.get('/bike', async (req, res) =>{
+            const query = {};
+            const cursor = bikeCollection.find(query);
+            const bikes = await cursor.toArray();
+            res.send(bikes);
+        })
+
+        app.get('/bike/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const bike = await bikeCollection.findOne(query);
+            res.send(bike);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 app.listen(port,()=>{
     console.log('Running port ', port)
 })
